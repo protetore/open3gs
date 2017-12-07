e058b0ec85170f77b5cd320f10e9194c0286450a
Diego <diego@commodityvectors.com>
2017-12-07 18:27:44 +0000

    Code formatting and fix usage function

--- functions.sh
+++ functions.sh
@@ -18,10 +18,8 @@ checkDependencies()
     retval=0;
 
     IFS=","
-    for dep in $DEPENDENCIES
-    do
-        if [ ! $(/usr/bin/which $dep) ];
-        then
+    for dep in $DEPENDENCIES; do
+        if [ ! $(/usr/bin/which $dep) ]; then
             $ECHO "ERROR: Dependency no found: $dep"
             retval=1
         fi
@@ -34,14 +32,12 @@ modemAttached()
 {
     # Find where modem is
     attachedDevice=$(dmesg | $GREP GSM | $GREP attached | $AWK '{ print $12 }' | $HEAD -1)
-    if [ "$attachedDevice" == "" ];
-    then
+    if [ "$attachedDevice" == "" ]; then
         $ECHO "ERROR: Modem not found. (1)";
         return 1;
     fi
 
-    if [ ! -f "/dev/$attachedDevice" ];
-    then
+    if [ ! -f "/dev/$attachedDevice" ]; then
         $ECHO "ERROR: Modem not found. (2)";
         return 1;
     fi
@@ -55,14 +51,12 @@ checkModem()
 {
     # Find where modem is
     attachedDevice=$(dmesg | $GREP GSM | $GREP attached | $AWK '{ print $12 }' | $HEAD -1)
-    if [ "$attachedDevice" == "" ];
-    then
+    if [ "$attachedDevice" == "" ]; then
         $ECHO "ERROR: Modem not found. (1)";
         return 1;
     fi
 
-    if [ ! -c "/dev/$attachedDevice" ];
-    then
+    if [ ! -c "/dev/$attachedDevice" ]; then
         $ECHO "ERROR: Modem not found. (2)";
         return 1;
     fi
@@ -88,16 +82,14 @@ checkModem()
     apnCode=$($CAT $LOG_DIR/modem.out)
     apnCode=$($ECHO "$apnCode" | $GREP "+COPS:" | $AWK -F"," '{ print $3 }' | $SED -e 's/"//g')
 
-    if [ "$apnCode" == "" ];
-    then
+    if [ "$apnCode" == "" ]; then
         $ECHO "ERROR: APN could not be detected! Modem sent an empty reply.";
         return 1;
     fi
 
     $confFile=$($GREP -l "COD=$apnCode" -r $BASE_DIR/$PROVIDERS_DIR/*.conf)
 
-    if [ "$confFile" == "" ];
-    then
+    if [ "$confFile" == "" ]; then
         $ECHO "ERROR: No config file for APN: $apnCode in $BASE_DIR/$PROVIDERS_DIR/";
         return 1;
     fi
@@ -112,15 +104,13 @@ reconfigure()
 {
     retval=0
 
-    if [ ! "$1" ==  "" ];
-    then
-        $ECHO "@ Loadind specific config..."
+    if [ ! "$1" ==  "" ]; then
+        $ECHO "@ Loadind specific config ($1)..."
         confFile=$1
     else
         $ECHO "@ Checking modem..."
         checkModem
-        if [ "$?" == "1" ];
-        then
+        if [ "$?" == "1" ]; then
             exit 1
         fi
 
@@ -131,10 +121,9 @@ reconfigure()
     $RM -f $BASE_DIR/$CONF_DIR/chat_default.conf
     $RM -f $BASE_DIR/$CONF_DIR/ppp_default.conf
 
-    if [ \( ! -f $confFile \) -a \( ! -f $BASE_DIR/$PROVIDERS_DIR/$confFile \) ];
-    then
+    if [ \( ! -f $confFile \) -a \( ! -f $BASE_DIR/$PROVIDERS_DIR/$confFile \) ]; then
         $ECHO "ERROR: Config file $confFile not found!"
-        exit 1;
+        retval=1
     fi
 
     #IFS="\n"
@@ -150,90 +139,58 @@ reconfigure()
     ATD=$($GREP -m 1 ATD "$confFile" | $SED '/^#.*$/d; s/ATD=//')
     COD=$($GREP -m 1 COD "$confFile" | $SED '/^#.*$/d; s/COD=//')
 
-    if [ "$APN" == "" ];
-    then
+    if [ "$APN" == "" ]; then
         $ECHO "ERROR: No APN (apn url) setting in $confFile!"
         retval=1
     fi
 
-    if [ "$USR" == "" ];
-    then
+    if [ "$USR" == "" ]; then
         $ECHO "ERROR: No USR (user) setting in $confFile!"
         retval=1
     fi
 
-    if [ "$PWD" == "" ];
-    then
+    if [ "$PWD" == "" ]; then
         $ECHO "ERROR: No PWD (password) setting in $confFile!"
         retval=1
     fi
 
-    if [ "$ATD" == "" ];
-    then
+    if [ "$ATD" == "" ]; then
         $ECHO "ERROR: No ATD (phone to dial) setting in $confFile!"
         retval=1
     fi
 
-    if [ "$COD" == "" ];
-    then
+    if [ "$COD" == "" ]; then
         $ECHO "ERROR: No COD (operator code) setting in $confFile!"
         retval=1
     fi
 
-    if [ "$retval" == "1" ];
-    then
-        exit 1
+    if [ ! -f $BASE_DIR/$CONF_DIR/pppd.conf ]; then
+        $ECHO "ERROR: PPPD config file $BASE_DIR/$CONF_DIR/pppd.conf not found!"
+        retval=1
     fi
 
-    if [ ! -f $BASE_DIR/$CONF_DIR/pppd.conf ];
-    then
-        $ECHO "ERROR: PPPD config file $BASE_DIR/$CONF_DIR/pppd.conf not found!"
-        exit 1;
+    if [ ! -f $BASE_DIR/$CONF_DIR/chat.conf ]; then
+        $ECHO "ERROR: CHAT config file $BASE_DIR/$CONF_DIR/chat.conf not found!"
+        retval=1
+    fi
+
+    if [ ! -f $BASE_DIR/$WVDIAL_DIR/template.conf ]; then
+        $ECHO "INFO: No template found in $BASE_DIR/$WVDIAL_DIR/template.conf..."
+        retval=1
+    fi
+
+    if [ "$retval" == "1" ]; then
+        exit 1
     fi
 
     PPP_OPTS=$($CAT $BASE_DIR/$CONF_DIR/pppd.conf | $TR '\n' ' ')
     PPP_OPTS=$($ECHO $PPP_OPTS | $SED "s|__MODEM__|$MODEM|g" | $SED "s/__USR__/$USR/g" | $SED "s/__PWD__/$PWD/g")
-
-    if [ ! -f $BASE_DIR/$CONF_DIR/chat.conf ];
-    then
-        $ECHO "ERROR: CHAT config file $BASE_DIR/$CONF_DIR/chat.conf not found!"
-        exit 1;
-    fi
 
     CHAT_OPTS=$($CAT $BASE_DIR/$CONF_DIR/chat.conf | $TR '\n' ' ')
     CHAT_OPTS=$($ECHO $CHAT_OPTS | $SED "s/__APN__/$APN/g" | $SED "s/__ATD__/$ATD/g")
 
     $ECHO $CHAT_OPTS > $BASE_DIR/$CONF_DIR/chat_default.conf
     $ECHO $PPP_OPTS > $BASE_DIR/$CONF_DIR/ppp_default.conf
-
-    if [ ! -f $BASE_DIR/$WVDIAL_DIR/template.conf ];
-    then
-        $ECHO "INFO: No template found. Using default settings..."
-
-        # $CAT > $BASE_DIR/$WVDIAL_DIR/template.conf <<- END_CFG
-        #     [Dialer Defaults]
-        #     Init1 = ATZ
-        #     Init2 = ATQ0 V1 E1 S0=0 &C1 &D2 +FCLASS=0
-        #     Init3 = AT+CGDCONT=1,"IP","__APN__"
-        #     Modem Type = Analog Modem
-        #     Phone = __ATD__
-        #     ISDN = 0
-        #     Username = __USR__
-        #     Password = __PWD__
-        #     Modem = __MODEM__
-        #     Baud = 460800
-        #     New PPPD = yes
-        #     Stupid Mode = yes
-        #     DialCommand = ATDT
-        #     Check Def Route = on
-        #     FlowControl = Hardware(CRTSCTS)
-        #     Auto Reconnect = on
-        #     Auto DNS = on
-        #     Abort on Busy = off
-        #     Carrier Check = off
-        #     Abort on No Dialtone = off
-        #     END_CFG
-    fi
 
     WVDIAL_OPTS=$($CAT $BASE_DIR/$WVDIAL_DIR/template.conf)
     WVDIAL_OPTS=$($ECHO $WVDIAL_OPTS | $SED "s|__MODEM__|$MODEM|g" | $SED "s/__USR__/$USR/g" | $SED "s/__PWD__/$PWD/g" | $SED "s/__APN__/$APN/g" | $SED "s/__ATD__/$ATD/g")
@@ -246,29 +203,32 @@ reconfigure()
 install()
 {
     $ECHO "@ SETUP"
+
+    if [ "$EUID" -ne 0 ]; then
+        $ECHO "Please execute install with root privileges"
+        exit 1
+    fi
+
     $ECHO "@ Checking dependencies..."
 
     checkDependencies
-    if [ "$?" == "1" ];
-    then
+    if [ "$?" == "1" ]; then
         exit 1
     fi
 
     $ECHO "@ Checking scripts..."
-    if [ "$0" == "/usr/bin/open3gs" ];
-    then
-            echo "ERROR: Setup function must be called from the sh script not from the link in /usr/bin/"
-            echo "Call: $BASE_DIR/open3gs setup"
-            exit 1
+    if [ "$0" == "/usr/bin/open3gs" ]; then
+        $ECHO "ERROR: Setup function must be called from the sh script not from the link in /usr/bin/"
+        $ECHO "Call: $BASE_DIR/open3gs install"
+        exit 1
     fi
 
     $ECHO "@ Configuring logrotate..."
     if [ -d /etc/logrotate.d ];
     then
-        if [ -f /etc/logrotate.d/vpn ];
-        then
-                $ECHO "INFO: Previous logrotate file found. Erasing it..."
-                $RM -f /etc/logrotate.d/open3gs
+        if [ -f /etc/logrotate.d/open3gs ]; then
+            $ECHO "INFO: Previous logrotate file found. Erasing it..."
+            $RM -f /etc/logrotate.d/open3gs
         fi
 
         # $CAT > /etc/logrotate.d/open3gs <<- END_CFG
@@ -276,33 +236,30 @@ install()
         #         missingok
         #         notifempty
         #         size 30k
-        #         monthly
+        #         weekly
         #         create 0600 root root
         #     }
         #     END_CFG
     else
-            $ECHO "INFO: Logrotate not found. Log will grow without supervision in $LOG_DIR"
+        $ECHO "INFO: Logrotate not found. Log will grow without supervision in $LOG_DIR"
     fi
 
     $ECHO "@ Creating logs directory..."
-    if [ ! -d $LOG_DIR ];
-    then
-            $MKDIR $LOG_DIR
+    if [ ! -d $LOG_DIR ]; then
+        $MKDIR $LOG_DIR
     else
-            $ECHO "INFO: Logs directory already exists. Skipping..."
+        $ECHO "INFO: Logs directory already exists. Skipping..."
     fi
 
-    if [ ! -h /usr/bin/open3gs  ];
-    then
-            $ECHO "@ Linking open3gs script to /usr/bin/..."
-            SCRIPT_PATH=$(/bin/readlink -f $0)
-            $LN -s $SCRIPT_PATH /usr/bin/open3gs
+    if [ ! -h /usr/bin/open3gs  ]; then
+        $ECHO "@ Linking open3gs script to /usr/bin/..."
+        SCRIPT_PATH=$(/bin/readlink -f $0)
+        $LN -s $SCRIPT_PATH /usr/bin/open3gs
     fi
 
     $ECHO "@ Detectiong modem and configuring defaut settings..."
     reconfigure
-    if [ "$?" == "1" ];
-    then
+    if [ "$?" == "1" ]; then
         exit 1
     fi
 
@@ -344,8 +301,7 @@ initConn()
 
 wvdial()
 {
-    if [ ! -f $BASE_DIR/$WVDIAL_DIR/default.conf ];
-    then
+    if [ ! -f $BASE_DIR/$WVDIAL_DIR/default.conf ]; then
         $ECHO "ERROR: WVDial default.conf config file not found. Run 'open3gs -r' or 'open3gs -i'. (3)"
         exit 1
     else
@@ -355,8 +311,8 @@ wvdial()
     initConn
 
     $ECHO "@ Dialing..."
-    nohup $WVDIAL -C $BASE_DIR/$WVDIAL_DIR/default.conf > $LOG_DIR/open3gs.log 2>&1 &
-
+    ppid=$(nohup $WVDIAL -C $BASE_DIR/$WVDIAL_DIR/default.conf > $LOG_DIR/open3gs.log 2>&1 & echo $!)
+    # writePID $ppid
     return 0
 }
 
@@ -374,25 +330,22 @@ openDial()
     "${command[@]}"
     cmdPid=$!
 
-    nohup connWatcher $cmdPid > $LOG_DIR/open3gs.log 2>&1 &
-
+    ppid=$(nohup connWatcher $cmdPid > $LOG_DIR/open3gs.log 2>&1 & echo $!)
+    # writePID $ppid
     exit 0
 }
 
 connWatcher()
 {
-    if [ "$1" == "" ];
-    then
+    if [ "$1" == "" ]; then
         $ECHO "ERROR: No PID to watch. Exiting now..."
         exit 1
     fi
 
-    while :
-    do
+    while :; do
         procNum=$($PS -ef | $GREP -v "grep" | $GREP "$1" | $GREP "pppd" | $WC -l)
 
-        if [ $procNum -eq 0 ];
-        then
+        if [ $procNum -eq 0 ]; then
             $ECHO "INFO: pppd process (PID $1) not found. Dialing again..."
             openDial
             exit 0
@@ -405,19 +358,16 @@ connWatcher()
 connect()
 {
     checkDependencies
-    if [ "$?" == "1" ];
-    then
+    if [ "$?" == "1" ]; then
         exit 1
     fi
 
     modemAttached
-    if [ "$?" == "1" ];
-    then
+    if [ "$?" == "1" ]; then
         exit 1
     fi
 
-    if [ "$1" == "0" ];
-    then
+    if [ "$1" == "0" ]; then
         reconfigure
         if [ ! $? ];
         then
@@ -425,8 +375,7 @@ connect()
         fi
     fi
 
-    if [ ! "$2" == "" ];
-    then
+    if [ ! "$2" == "" ]; then
         reconfigure $2
         if [ ! $? ];
         then
@@ -434,20 +383,17 @@ connect()
         fi
     fi
 
-    if [ ! -f $BASE_DIR/$CONF_DIR/chat_default.conf ];
-    then
+    if [ ! -f $BASE_DIR/$CONF_DIR/chat_default.conf ]; then
         $ECHO "ERROR: Config file not found. Run 'open3gs -r' or 'open3gs -i'. (1)"
         exit 1
     fi
 
-    if [ ! -f $BASE_DIR/$CONF_DIR/ppp_default.conf ];
-    then
+    if [ ! -f $BASE_DIR/$CONF_DIR/ppp_default.conf ]; then
         $ECHO "ERROR: Config file not found. Run 'open3gs -r' or 'open3gs -i'. (2)"
         exit 1
     fi
 
-    if [ "$3" == "0" ];
-    then
+    if [ "$3" == "0" ]; then
         $ECHO "@ Using experimental connection mode (no third party dialer)..."
         openDial
     else
@@ -485,27 +431,33 @@ connStatus()
 
 usage()
 {
+    echo ">> Usage: open3gs [command] [option|optional]"
     echo
-    echo ">> Usage: open3gs [option|optional]"
+    echo "COMMANDS"
+    echo "--------"
+    echo " install      First step: create link to bin, logs dir, configure logrotate, detect modem, carrier..."
+    echo " status       Status of the connection"
+    echo " connect      Connect to the auto-detected carrier"
+    echo " disconnect   Close active connection"
+    echo " reconnect    Finishes the actual connection/process and try to connect again"
+    echo " reconfigure  Detect the modem and APN and build the default configuration file"
+    echo " help         Display this screen"
     echo
-    echo " -i         First step: create link to bin, logs dir, configure logrotate, detect modem, carrier..."
-    echo " -s         Status of the connection"
-    echo " -c         Connect to the auto-detected carrier"
-    echo " -d         Close active connection"
-    echo " -r         Finishes the actual connection/process and try to connect again."
-    echo " -n         Detect the modem and APN and build the default configuration file."
-    echo " -a         Always detect modem, APN, configure the modem when dialing. (Use with the -c option)"
-    echo " -e         Experimental dialing option. Don't use wvdial or other third party tools, use only pppd and basic linux tools."
-    echo " -f         Force to use a specific config file. (Use with -n or -c options)."
-    echo " -h         Display this screen"
+    echo "OPTIONS"
+    echo "-------"
+    echo " -a         Always detect modem, APN, configure the modem when dialing (Use with the -c option)"
+    echo " -e         Experimental dialing option, don't use wvdial or other third party tools, use only pppd and basic linux tools"
+    echo " -f         Force to use a specific config file (Use with -n or -c options)."
+    # echo " -p         PID file. Useful when running more than one instance of open3gs"
     echo
-    echo "---------"
-    echo "Examples: "
-    echo "---------"
-    echo "open3gs -c -f                                    #connect using settings created by 'open3gs -i' or 'open3gs -n'"
-    echo "open3gs -c -f /opt/open3gs/providers.d/oi.conf   #connect using specific config file"
-    echo "open3gs -c -a                                    #aways try to detect carrier and modem when dialing"
-    echo "open3gs -n                                       #config open3gs auto-detecting settings"
-    echo "open3gs -n -f /opt/open3gs/providers.d/oi.conf   #config open3gs to use specific config file"
+    echo "EXAMPLES"
+    echo "--------"
+    echo " Connect using settings created by 'install' or 'reconfigure' or using specific config file"
+    echo " > open3gs connect"
+    echo " > open3gs connect -f /opt/open3gs/providers.d/oi.conf"
+    echo
+    echo " Configure open3gs auto-detecting settings or with specific file:"
+    echo " > open3gs reconfigure"
+    echo " > open3gs reconfigure -f /opt/open3gs/providers.d/oi.conf"
     echo
 }
